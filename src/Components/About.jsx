b1543680f054f9a8fa22cc97de51040998c03464
Harsh Jain <harshuu10@live.com>
2024-01-03 22:13:41 +0530

    add all changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -18,25 +18,25 @@ import React from "react";
  */
 import image from "../images/scene.jpg";
 
-const imageAltText = "green park with beautiful background";
+const imageAltText = "Green park and trees with beautiful background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Science student studying at B.P. Poddar Institute of Management and Technology. I love creating Websites and clicking nature photographs.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web Designing",
+  "Python with Machine Learning",
+  "Artificial Intelligence",
+  "Database Management System",
+  "Esports Gaming",
+  "Graphic Designing",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about software developers with expertise in C++, Java, Python. Committed to continuous learning, collaboration, and innovative projects.";
 
 const About = () => {
   return (
