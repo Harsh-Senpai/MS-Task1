43ce4fe0a9bd9ffe56f90cb41bdc58161a8c1b2a
Harsh Jain <harshuu10@live.com>
2024-01-03 21:21:10 +0530

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Harsh Jain",
+  title: "Web Developer & Photography",
+  email: "Harsh.Jain@studentambassadors.com",
+  gitHub: "Harsh-Senpai",
+  instagram: "snap.freak__",
+  linkedIn: "harsh-j-b8aa0583",
   medium: "",
-  twitter: "microsoft",
+  twitter: "InoriMaster11",
   youTube: "Code",
 };
 
